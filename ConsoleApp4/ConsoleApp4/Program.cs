4f1b09b4af0af3a30c4f6f80f272abc96c2403c7
lucas-abratti <lucasabratti99@gmail.com>
2023-08-24 11:58:14 -0300

    search

--- ConsoleApp4/ConsoleApp4/Program.cs
+++ ConsoleApp4/ConsoleApp4/Program.cs
@@ -1,5 +1,7 @@
-﻿using System.Diagnostics;
+﻿using System.Data;
+using System.Diagnostics;
 using System.IO;
+using System.Reflection;
 
 namespace ConsoleApp4
 {
@@ -8,14 +10,10 @@ namespace ConsoleApp4
         static void Main(string[] args)
         {
 
-            string[] options = new string[] {"[1] Escribir archivo", "[2] Leer archivo", "[3] Cerrar programa" };
+            string[] options = new string[] {"[1] Escribir archivo", "[2] Leer archivo", "[3] Buscar", "[3] Cerrar programa" };
             bool quitProgram = false;
             int currentSelection = 0;
             ConsoleKey key;
-
-            FileStream archivo;
-            StreamWriter grabador;
-            StreamReader lector;
             do
             {
                 do
@@ -47,30 +45,20 @@ namespace ConsoleApp4
                 switch (currentSelection)
                 {
                     case 0:
-                        archivo = new FileStream("content.txt", FileMode.Append);
-                        grabador = new StreamWriter(archivo);
+                        do
+                        {
+                            WriteFile();
+                            Console.WriteLine("Desea Ingresar otro alumno? [si]");
 
-                        Console.WriteLine("Ingrese algo\n");
-                        string ingreso = Console.ReadLine();
-
-                        grabador.WriteLine(ingreso);
-                        grabador.Close();
+                        } while (Console.ReadLine() == "si");
                         break;
                     case 1:
-                        archivo = new FileStream("content.txt", FileMode.Open);
-                        lector = new StreamReader(archivo);
-
-                        while (lector.EndOfStream == false)
-                        {
-                            string cadena = lector.ReadLine();
-                            Console.WriteLine(cadena);
-                        }
-
-                        lector.Close();
-                        Console.ReadKey();
-                        archivo.Close();    
+                        ShowFormatedFile();
                         break;
                     case 2:
+                        SearchInFile();
+                        break;
+                    case 3:
                         Console.WriteLine("Cerrando Programa");
                         quitProgram = true;
                         break;
@@ -78,7 +66,130 @@ namespace ConsoleApp4
 
                 Console.Clear();
 
-            } while (!quitProgram);        
+            } while (!quitProgram);
+        }
+        static void IngresoAlumno(out int edad, out string nombre, out string apellido, out string direccion, out int legajo)
+        {
+            bool parsedSuccessfully = false;
+
+            do
+            {
+                Console.WriteLine("\nIngrese la edad del alumno\n");
+                parsedSuccessfully = int.TryParse(Console.ReadLine(), out edad);
+
+            } while (!parsedSuccessfully);
+
+            Console.WriteLine("\nIngrese el nombre del alumno\n");
+            nombre = Console.ReadLine();
+            Console.WriteLine("\nIngrese el nombre del apellido\n");
+            apellido = Console.ReadLine();
+            Console.WriteLine("\nIngrese la direccion del alumno\n");
+            direccion = Console.ReadLine();
+
+            parsedSuccessfully = false;
+            do
+            {
+                Console.WriteLine("\nIngrese el numero de lagajo del alumno\n");
+                parsedSuccessfully = int.TryParse(Console.ReadLine(), out legajo);
+
+            } while (!parsedSuccessfully);
+        
+        }
+
+        static string FormateoRenglon(string renglon)
+        {
+            const int PAD = 10;
+            
+            string[] valores = renglon.Split(';');
+            string renglonFormateado = "";
+            foreach(string s in valores)
+            {
+                renglonFormateado += $"{s.PadLeft(PAD)} |";
+            }
+            return renglonFormateado;
+        }
+        static void SearchInFile()
+        {
+            int legajo;
+            bool parsedSuccessfully = false;
+
+            do
+            {
+                Console.WriteLine("Ingrese legajo a buscar\n");
+                parsedSuccessfully = int.TryParse(Console.ReadLine(), out legajo);
+
+            } while (!parsedSuccessfully);
+            
+
+            string legajoS = GetAlumnoFromLegajo(4, legajo.ToString());
+
+            if (legajoS == "")
+            {
+                Console.WriteLine("Legajo no encontrado");
+            }
+            else
+            {
+                Console.WriteLine(FormateoRenglon(legajoS));
+            }
+            Console.ReadKey();
+        }
+
+        static void WriteFile()
+        {
+            FileStream archivo;
+            StreamWriter grabador;
+
+            archivo = new FileStream("content.csv", FileMode.Append);
+            grabador = new StreamWriter(archivo);
+
+            IngresoAlumno(out int edad, out string nombre, out string apellido, out string direccion, out int legajo);
+            string ingreso = $"{edad};{nombre};{apellido};{direccion};{legajo}";
+            grabador.WriteLine(ingreso);
+            grabador.Close();
+        }
+
+        static void ShowFormatedFile()
+        {
+            const string NOMBRE = "content.csv";
+            FileStream archivo = new FileStream("content.csv", FileMode.Open);
+            StreamReader lector = new StreamReader(archivo);
+
+            if (!File.Exists(NOMBRE)) { Console.WriteLine($"Archivo inexistente: {NOMBRE}"); Console.ReadKey(); return; }
+
+
+
+            while (lector.EndOfStream == false)
+            {
+                string cadena = FormateoRenglon(lector.ReadLine());
+                Console.WriteLine(cadena);
+            }
+
+            lector.Close();
+            archivo.Close();
+            Console.ReadKey();
+        }
+        static string GetAlumnoFromLegajo(int index, string value)
+        {
+            const string NOMBRE = "content.csv";
+            FileStream archivo = new FileStream("content.csv", FileMode.Open);
+            StreamReader lector = new StreamReader(archivo);
+
+            if (!File.Exists(NOMBRE)) { Console.WriteLine($"Archivo inexistente: {NOMBRE}"); Console.ReadKey(); return ""; }
+
+            while (lector.EndOfStream == false)
+            {
+                string renglon = lector.ReadLine();
+                string[] valores = renglon.Split(';');
+                if (valores[index] == value)
+                {
+                    lector.Close();
+                    archivo.Close();
+                    return renglon;
+                }
+            }
+            lector.Close();
+            archivo.Close();
+            return "";
         }
     }
 }
